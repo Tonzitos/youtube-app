7c22f4f00f65990cc74301a413c90af0f003265d
tonzitos <wramos.contact@gmail.com>
2020-09-06 21:56:25 +0100

    get youtube data by curl and display it in the frontend

--- module/YouTubeApplication/src/Controller/YouTubeApplicationController.php
+++ module/YouTubeApplication/src/Controller/YouTubeApplicationController.php
@@ -14,14 +14,41 @@ class YouTubeApplicationController extends AbstractActionController
 
         if($request->isPost()){
             $search = $request->getPost('search');
+            $search = preg_replace('/[^A-Za-z0-9\-]/', '', $search);
+
+            $videoData = $this->getYoutubeVideos($search);
+
+            if($videoData){
+                $this->getResponse()->setStatusCode(200);
+                $this->getResponse()->setContent(json_encode(array('result'=>'true', 'data'=>$videoData)));
+                return $this->getResponse();
+            }
+
             $this->getResponse()->setStatusCode(200);
-            $this->getResponse()->setContent(json_encode(array('result'=>'true', 'message'=>'good')));
+            $this->getResponse()->setContent(json_encode(array('result'=>'false', 'message'=>'Something is not correct, we are working to fix it ASAP!')));
             return $this->getResponse();
 
         }
     }
 
+    protected function getYoutubeVideos($keyword){
+        $apikey = 'AIzaSyB6xKCkXsDTUHqaUfR3JafT2LIpGuaUAIo';
+        $googleApiUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&q=' . $keyword . '&maxResults=' . 10 . '&key=' . $apikey . '&regionCode=IE';
 
+        $ch = curl_init();
 
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_VERBOSE, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($ch);
 
+        curl_close($ch);
+        $data = json_decode($response);
+        $value = json_decode(json_encode($data), true);
+
+        return $value;
+    }
 }
