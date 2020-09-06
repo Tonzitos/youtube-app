d967b9d35ca81237f2233d7a13ffb5d8cf14c5d9
tonzitos <wramos.contact@gmail.com>
2020-09-06 20:16:28 +0100

    prepare app to start googleapi

--- module/YouTubeApplication/src/Controller/YouTubeApplicationController.php
+++ module/YouTubeApplication/src/Controller/YouTubeApplicationController.php
@@ -10,6 +10,18 @@ class YouTubeApplicationController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
+        $request = $this->getRequest();
+
+        if($request->isPost()){
+            $search = $request->getPost('search');
+            $this->getResponse()->setStatusCode(200);
+            $this->getResponse()->setContent(json_encode(array('result'=>'true', 'message'=>'good')));
+            return $this->getResponse();
+
+        }
     }
+
+
+
+
 }
